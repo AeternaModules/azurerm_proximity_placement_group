5aa9d5bdda61cbd6efc0aff1ade2949acfbce428
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "proximity_placement_groups" {
-  description = "All proximity_placement_group resources"
-  value       = azurerm_proximity_placement_group.proximity_placement_groups
-}
 output "proximity_placement_groups_allowed_vm_sizes" {
-  description = "List of allowed_vm_sizes values across all proximity_placement_groups"
-  value       = [for k, v in azurerm_proximity_placement_group.proximity_placement_groups : v.allowed_vm_sizes]
+  description = "Map of allowed_vm_sizes values across all proximity_placement_groups, keyed the same as var.proximity_placement_groups"
+  value       = { for k, v in azurerm_proximity_placement_group.proximity_placement_groups : k => v.allowed_vm_sizes }
 }
 output "proximity_placement_groups_location" {
-  description = "List of location values across all proximity_placement_groups"
-  value       = [for k, v in azurerm_proximity_placement_group.proximity_placement_groups : v.location]
+  description = "Map of location values across all proximity_placement_groups, keyed the same as var.proximity_placement_groups"
+  value       = { for k, v in azurerm_proximity_placement_group.proximity_placement_groups : k => v.location }
 }
 output "proximity_placement_groups_name" {
-  description = "List of name values across all proximity_placement_groups"
-  value       = [for k, v in azurerm_proximity_placement_group.proximity_placement_groups : v.name]
+  description = "Map of name values across all proximity_placement_groups, keyed the same as var.proximity_placement_groups"
+  value       = { for k, v in azurerm_proximity_placement_group.proximity_placement_groups : k => v.name }
 }
 output "proximity_placement_groups_resource_group_name" {
-  description = "List of resource_group_name values across all proximity_placement_groups"
-  value       = [for k, v in azurerm_proximity_placement_group.proximity_placement_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all proximity_placement_groups, keyed the same as var.proximity_placement_groups"
+  value       = { for k, v in azurerm_proximity_placement_group.proximity_placement_groups : k => v.resource_group_name }
 }
 output "proximity_placement_groups_tags" {
-  description = "List of tags values across all proximity_placement_groups"
-  value       = [for k, v in azurerm_proximity_placement_group.proximity_placement_groups : v.tags]
+  description = "Map of tags values across all proximity_placement_groups, keyed the same as var.proximity_placement_groups"
+  value       = { for k, v in azurerm_proximity_placement_group.proximity_placement_groups : k => v.tags }
 }
 output "proximity_placement_groups_zone" {
-  description = "List of zone values across all proximity_placement_groups"
-  value       = [for k, v in azurerm_proximity_placement_group.proximity_placement_groups : v.zone]
+  description = "Map of zone values across all proximity_placement_groups, keyed the same as var.proximity_placement_groups"
+  value       = { for k, v in azurerm_proximity_placement_group.proximity_placement_groups : k => v.zone }
 }
 
